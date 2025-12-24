b8711499436d2812224f516c5075f4e394c95f71
Redie-tes <rediettesfayefetenu@gmail.com>
2025-12-24 12:52:54 +0300

    Add function to print full name with greeting

---
+++ what's your name.py
@@ -0,0 +1,7 @@
+def print_full_name(first, last):
+    # Write your code here
+    a = "Hello"
+    b = first_name
+    c = last_name
+    d = "You just delved into python."
+    print(f"{a} {b} {c}! {d}")
